aec7febc80669a36f3e012144d36b9705671f851
Programming Hero <programming.hero1@gmail.com>
2024-09-18 17:30:54 -0500

    created utilties function for multiple calls

--- js/addMoney.js
+++ js/addMoney.js
@@ -6,14 +6,22 @@
  * 4. parseFloat
 */
 
-console.log('add money loaded');
+// console.log('add money loaded');
 document.getElementById('btn-add-money')
     .addEventListener('click', function (event) {
         event.preventDefault();
 
         console.log('add money button clicked');
+
+        // getInputFieldValueById();
+        // const addMoney = getInputFieldValueById();
+        // console.log('add money value', addMoney);
         // const addMoney = document.getElementById('input-add-money').value;
         // const addMoneyNumber = parseFloat(addMoney);
+
+        const addMoney = getInputFieldValueById('input-add-money');
+        const pinNumber = getInputFieldValueById('input-pin-number');
+        console.log('add money with parameter', addMoney, pinNumber)
 
 
     });
--- js/utilities.js
+++ js/utilities.js
@@ -1,9 +1,17 @@
 /**
  * Common Shared functions here
 */
-console.log('utilities loaded');
+// console.log('utilities loaded');
 
 
-function getInputFieldValueById(){
-    console.log('will get value by id');
+// function getInputFieldValueById(){
+//     console.log('will get value by id');
+//     const addMoney = document.getElementById('input-add-money').value;
+//     return addMoney;
+// }
+
+
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
 }
